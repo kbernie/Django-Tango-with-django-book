be78445a9349c1eea22ede764ceba401bdb6145d
BernieK <koltaibernie@gmail.com>
2017-09-28 23:16:45 +0200

    4 - Data driven page: how to use models-views-templates to show data from DB, slugify urls, regex

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -5,5 +5,11 @@ from rango.models import Category, Page
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
-admin.site.register(Category)
+
+# it automatically prepopulates the slug field as you type in the category name
+# so it can be savedc | no need to type it:
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True) # to make sure we don't have the same category name twice
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -21,3 +27,9 @@ class Page(models.Model):
 
     def __str__(self): # For Python 2, use __unicode__ too
         return self.title
+
+
+
+
+
+        
